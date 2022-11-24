2480bd0bcd5ebc5d2e0624d5b740d4c98b300dec
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2022-11-24 15:18:18 -0300

    Diseño carrito

--- Vista/compra/carrito.php
+++ Vista/compra/carrito.php
@@ -24,20 +24,23 @@ include ($dir."../estructura/header.php");
                                     <div class="row">
                                         <div class="col-md-5 product-name">
                                             <div class="product-name">
-                                                <a href="#">Lorem Ipsum dolor</a>
+                                                <a href="#">Camara Hikvision 5mpx</a>
                                                 <div class="product-info">
-                                                    <div>Display: <span class="value">5 inch</span></div>
-                                                    <div>RAM: <span class="value">4GB</span></div>
-                                                    <div>Memory: <span class="value">32GB</span></div>
+                                                    <div>Marca: <span class="value">Hikvision</span></div>
+                                                    <div>Disponibles: <span class="value">11</span></div>
+                                                    
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-4 quantity">
-                                            <label for="quantity">Quantity:</label>
+                                        <div class="col-md-3 quantity">
+                                            <label for="quantity">Cantidad:</label>
                                             <input id="quantity" type="number" value ="1" class="form-control quantity-input">
                                         </div>
-                                        <div class="col-md-3 price">
-                                            <span>$120</span>
+                                        <div class="col-md-2 price">
+                                            <span>$34000</span>
+                                        </div>
+                                        <div class="col-md-2 pl-2">
+                                            <button class="btn btn-danger">Eliminar</button>
                                         </div>
                                     </div>
                                 </div>
@@ -47,64 +50,72 @@ include ($dir."../estructura/header.php");
                     <div class="product">
                         <div class="row">
                             <div class="col-md-3 d-flex">
-                                <img class="img-fluid mx-auto d-flex image" src="https://i.ibb.co/SXwW8g6/E1.webp">
+                            <img class="img-fluid mx-auto d-flex image" src="https://i.ibb.co/SXwW8g6/E1.webp">
                             </div>
                             <div class="col-md-8">
                                 <div class="info">
                                     <div class="row">
                                         <div class="col-md-5 product-name">
                                             <div class="product-name">
-                                                <a href="#">Lorem Ipsum dolor</a>
+                                                <a href="#">DVR dahua 4108XD</a>
                                                 <div class="product-info">
-                                                    <div>Display: <span class="value">5 inch</span></div>
-                                                    <div>RAM: <span class="value">4GB</span></div>
-                                                    <div>Memory: <span class="value">32GB</span></div>
+                                                    <div>Marca: <span class="value">Dahua</span></div>
+                                                    <div>Disponibles: <span class="value">10</span></div>
+                                                    
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-4 quantity">
-                                            <label for="quantity">Quantity:</label>
+                                        <div class="col-md-3 quantity">
+                                            <label for="quantity">Cantidad:</label>
                                             <input id="quantity" type="number" value ="1" class="form-control quantity-input">
                                         </div>
-                                        <div class="col-md-3 price">
-                                            <span>$120</span>
+                                        <div class="col-md-2 price">
+                                            <span>$25000</span>
+                                        </div>
+                                        <div class="col-md-2 pl-2">
+                                            <button class="btn btn-danger">Eliminar</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                  
+                  
                     <div class="product">
                         <div class="row">
-                            <div class="col-md-3 d-flex"width="300px" height="">
-                             
-                                <img class="img-fluid mx-auto d-flex image" src="https://i.ibb.co/V2TFP2R/C6.webp" >
-                             </div>
+                            <div class="col-md-3 d-flex">
+                            <img class="img-fluid mx-auto d-flex image" src="https://i.ibb.co/V2TFP2R/C6.webp" >
+                            </div>
                             <div class="col-md-8">
                                 <div class="info">
                                     <div class="row">
                                         <div class="col-md-5 product-name">
                                             <div class="product-name">
-                                                <a href="#">Lorem Ipsum dolor</a>
+                                                <a href="#">Camara dahua 5mpx</a>
                                                 <div class="product-info">
-                                                    <div>Display: <span class="value">5 inch</span></div>
-                                                    <div>RAM: <span class="value">4GB</span></div>
-                                                    <div>Memory: <span class="value">32GB</span></div>
+                                                    <div>Marca: <span class="value">Dahua</span></div>
+                                                    <div>Disponibles: <span class="value">9</span></div>
+                                                    
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-4 quantity">
-                                            <label for="quantity">Quantity:</label>
+                                        <div class="col-md-3 quantity">
+                                            <label for="quantity">Cantidad:</label>
                                             <input id="quantity" type="number" value ="1" class="form-control quantity-input">
                                         </div>
-                                        <div class="col-md-3 price">
-                                            <span>$120</span>
+                                        <div class="col-md-2 price">
+                                            <span>$32000</span>
+                                        </div>
+                                        <div class="col-md-2 pl-2">
+                                            <button class="btn btn-danger">Eliminar</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                   
                 </div>
             </div>
             <div class="col-md-12 col-lg-4">
